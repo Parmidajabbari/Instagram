b21ab0a94cc373f84d3b480eac28401f137ba7ab
Parmidajabbari <parmidajabbari@gmail.com>
2021-06-09 00:45:06 +0430

    Sign Up and Login edited

--- src/controllers/SignUpController.java
+++ src/controllers/SignUpController.java
@@ -1,24 +1,35 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class SignUpController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUpController implements Initializable {
+    @FXML
     public void enterEmail(ActionEvent actionEvent) {
     }
-
+    @FXML
     public void nextPage(ActionEvent actionEvent) {
     }
-
+    @FXML
     public void signIn(ActionEvent actionEvent) {
     }
-
+    @FXML
     public void back(ActionEvent actionEvent) throws Exception {
         PageController.closePage(actionEvent);
         //PageController.openPage("mainPage");
     }
-
+    @FXML
     public void close(ActionEvent actionEvent) {
         PageController.closePage(actionEvent);
         System.exit(1);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
--- src/controllers/ThirdSignUpController.java
+++ src/controllers/ThirdSignUpController.java
@@ -1,14 +1,25 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class ThirdSignUpController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ThirdSignUpController implements Initializable {
+    @FXML
     public void setUserName(ActionEvent actionEvent) {
     }
-
+    @FXML
     public void nextPage(ActionEvent actionEvent) {
     }
-
+    @FXML
     public void close(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
