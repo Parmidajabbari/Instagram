1aabe891c4fab7cde397f4e8406c77c74b345c7e
Parmidajabbari <parmidajabbari@gmail.com>
2021-06-28 17:22:24 +0430

    image to string

--- src/controllers/NewPostController.java
+++ src/controllers/NewPostController.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import app.Post;
+import app.Tasks;
+import com.sun.xml.internal.messaging.saaj.util.Base64;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,7 +13,12 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -36,9 +43,14 @@ public class NewPostController implements Initializable {
         return (dotIndex == -1) ? "" : fileName.substring(dotIndex + 1);
     }
 
-
+    private static String encodeFileToBase64Binary(File file) throws Exception{
+        FileInputStream fileInputStreamReader = new FileInputStream(file);
+        byte[] bytes = new byte[(int)file.length()];
+        fileInputStreamReader.read(bytes);
+        return new String(Base64.encode(bytes), "UTF-8");
+    }
     @FXML
-    protected void setPhoto(ActionEvent event) {
+    protected void setPhoto(ActionEvent event) throws Exception {
         FileChooser chooser = new FileChooser();
         chooser.setTitle("Open File");
         File file = chooser.showOpenDialog(new Stage());
@@ -47,6 +59,8 @@ public class NewPostController implements Initializable {
             image = new Image(file.toURI().toString());
             newPostPhoto.setImage(image);
             isSet = true;
+            String imageString = encodeFileToBase64Binary(file);
+           // String massage =
         }
         else {
             return;
