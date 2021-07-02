bc2e112ca5589065358454d3475e179fa3bd1233
Parmidajabbari <parmidajabbari@gmail.com>
2021-07-02 09:11:12 +0430

    graphic edits

--- src/controllers/HomePagePostController.java
+++ src/controllers/HomePagePostController.java
@@ -56,6 +56,7 @@ public class HomePagePostController implements Initializable {
         String massage = Tasks.getLikeTask(Integer.toString(LoginPageController.getUserId()), Integer.toString(postId));
         Client.sendRequest(massage);
         likeButton.setStyle("-fx-background-color: red;");
+        likesCount.setText(Integer.toString(post.getLikes()+1));
     }
 
     @FXML
